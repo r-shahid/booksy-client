66868233a7f45a3c0f4a1e72adb046cebd1e07a7
Riana <rianashahid@gmail.com>
2020-11-29 00:46:06 -0500

    done

--- src/components/About.js
+++ src/components/About.js
@@ -14,30 +14,32 @@ const About = () => {
 				</div>
 
 				<div className='about-info'>
-					<span>Booksy is the lovechild of a Trello board and Goodreads.</span>{' '}
-					It's intended to be used as a book-tracking app where users can add
+					<span>Booksy is the lovechild of a Trello board and Goodreads. </span>
+					It's intended to serve as a book-tracking app where users can add
 					books they want to read, books they're currently reading, and books
-					they have read. Once a book is moved into the "Read" list, users can
+					they have read. New books are added to the left-most column and can be moved by clicking the button next to the trash icon. Once a book is moved into the "Read" list, users can
 					add a rating.
+					<br />
+					I made Booksy because I'm an avid reader. Back when I did marketing for a publisher, I made a few friends in the industry, who kindly still send me books to review. That, paired with the fact that I'm constantly buying new books means that I always have a steady supply of unread books. Using an app like Booksy helps me keep track of everything on my shelf. 
 					<hr className='above-container' />
 					<div class='container'>
 						<img src={booksy} alt='Avatar' class='image' />
 						<div class='overlay'>
 							<div class='text'>
-								See what Booksy almost looked like <br />
+								See Booksy's first design <br />
 								<i class='material-icons'>remove_red_eye</i>
 							</div>
 						</div>
 					</div>
 					<hr />
-					Additional functionalities coming soon:
+					<b>Additional functionalities coming soon:</b>
 					<ul class='browser-default'>
 						<li>thumbnail images for each book</li>
 						<li>
 							option to select books from a changing list of popular titles
 						</li>
 						<li>progress bar for books in the "Reading" list</li>
-						<li>start and end dates</li>
+						<li>start date, estimated end date, actual end date</li>
 						<li>
 							a dark color palette that users can toggle with a switch and the
 							option to turn off animations
@@ -56,10 +58,11 @@ const About = () => {
 					thread. Given the ongoing pandemic, I have been working through my
 					yarn stash by knitting socks for myself and my family.
 					<hr />
-					See my previous project here:{' '}
-					<a href='https://rs-tunr.netlify.app/'>Tunr</a>, a playlist app with a
-					neon/vaporwave theme. See all my past work{' '}
-					<a href='https://r-shahid.github.io'>here</a>.
+					Previous project:
+					<a href='https://rs-tunr.netlify.app/'> Tunr</a>, a playlist app with a
+					neon/vaporwave theme. 
+					<br />See all my past work
+					<a href='https://r-shahid.github.io'> here</a>.
 					<hr />
 					Email: <a href='mailto:info@rianashahid.com'>info@rianashahid.com</a>
 				</div>
